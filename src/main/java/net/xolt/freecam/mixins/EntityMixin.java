f52cba7be4f6cf5e007f3244a7aba0e11c225ea1
hashalite <hashalite@gmail.com>
2022-02-11 00:13:47 -0500

    Ok Zoomer compatibility

---
+++ src/main/java/net/xolt/freecam/mixins/EntityMixin.java
@@ -0,0 +1,23 @@
+package net.xolt.freecam.mixins;
+
+import net.minecraft.entity.Entity;
+import net.xolt.freecam.Freecam;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.xolt.freecam.Freecam.MC;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+
+    @Inject(method = "changeLookDirection", at = @At("HEAD"), cancellable = true)
+    private void onChangeLookDirection(double x, double y, CallbackInfo ci) {
+        if (Freecam.isEnabled() && this.equals(MC.player)) {
+            Freecam.getFreeCamera().changeLookDirection(x, y);
+            ci.cancel();
+        }
+    }
+
+}
