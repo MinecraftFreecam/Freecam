5508da65546fcf6eaaf8ef48685bcacd706f101a
hashalite <hashalite@gmail.com>
2022-02-11 22:47:53 -0500

    1.16 compatibility

--- src/main/java/net/xolt/freecam/mixins/BlockEntityMixin.java
+++ src/main/java/net/xolt/freecam/mixins/BlockEntityMixin.java
@@ -1,19 +1,19 @@
 package net.xolt.freecam.mixins;
 
-import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.block.entity.BlockEntity;
 import net.xolt.freecam.Freecam;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BlockEntityRenderer.class)
-public interface BlockEntityRendererMixin {
+@Mixin(BlockEntity.class)
+public abstract class BlockEntityMixin {
 
-    @Inject(method = "isInRenderDistance", at = @At("HEAD"), cancellable = true)
-    private void onIsInRenderDistance(CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "getRenderDistance", at = @At("HEAD"), cancellable = true)
+    private void onGetRenderDistance(CallbackInfoReturnable<Double> cir) {
         if (Freecam.isEnabled()) {
-            cir.setReturnValue(true);
+            cir.setReturnValue(1000D);
         }
     }
 }
