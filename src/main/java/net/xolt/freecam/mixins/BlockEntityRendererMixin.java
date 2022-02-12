b77620e95cc5c40d99e7668013b2c8af6cf88511
hashalite <hashalite@gmail.com>
2022-02-11 19:10:02 -0500

    - Made entities and block entities outside of entity view distance render while freecam is enabled

---
+++ src/main/java/net/xolt/freecam/mixins/BlockEntityRendererMixin.java
@@ -0,0 +1,20 @@
+package net.xolt.freecam.mixins;
+
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.xolt.freecam.Freecam;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockEntityRenderer.class)
+public interface BlockEntityRendererMixin {
+
+    @Inject(method = "isInRenderDistance", at = @At("HEAD"), cancellable = true)
+    private void onIsInRenderDistance(CallbackInfoReturnable<Boolean> cir) {
+        if (Freecam.isEnabled()) {
+            cir.setReturnValue(true);
+        }
+    }
+
+}
--- src/main/java/net/xolt/freecam/mixins/EntityMixin.java
+++ src/main/java/net/xolt/freecam/mixins/EntityMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static net.xolt.freecam.Freecam.MC;
 
@@ -17,6 +18,13 @@ public class EntityMixin {
         if (Freecam.isEnabled() && this.equals(MC.player)) {
             Freecam.getFreeCamera().changeLookDirection(x, y);
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "shouldRender(D)Z", at = @At("HEAD"), cancellable = true)
+    private void onShouldRender(CallbackInfoReturnable<Boolean> cir) {
+        if (Freecam.isEnabled()) {
+            cir.setReturnValue(true);
         }
     }
 
