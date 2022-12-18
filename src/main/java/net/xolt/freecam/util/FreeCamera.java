fdddfca3cbd0b040821c69efdcca867fdecd9c5f
hashalite <hashalite@gmail.com>
2022-12-18 03:06:12 -0500

    - Added #52 - Improved performance of tripods

--- src/main/java/net/xolt/freecam/util/FreeCamera.java
+++ src/main/java/net/xolt/freecam/util/FreeCamera.java
@@ -26,11 +26,15 @@ public class FreeCamera extends ClientPlayerEntity {
     };
 
     public FreeCamera(int id) {
+        this(id, new FreecamPosition(MC.player));
+    }
+
+    public FreeCamera(int id, FreecamPosition position) {
         super(MC, MC.world, NETWORK_HANDLER, MC.player.getStatHandler(), MC.player.getRecipeBook(), false, false);
 
         setId(id);
-        copyPositionAndRotation(MC.player);
-        super.setPose(MC.player.getPose());
+        refreshPositionAndAngles(position.x, position.y, position.z, position.yaw, position.pitch);
+        super.setPose(position.pose);
         renderPitch = getPitch();
         renderYaw = getYaw();
         lastRenderPitch = renderPitch; // Prevents camera from rotating upon entering freecam.
