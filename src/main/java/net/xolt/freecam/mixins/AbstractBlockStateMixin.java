b75af36b33d28416007e005473613bc46d5bfdb2
hashalite <hashalite@gmail.com>
2023-06-16 20:02:41 -0400

    Fixed #90

--- src/main/java/net/xolt/freecam/mixins/AbstractBlockStateMixin.java
+++ src/main/java/net/xolt/freecam/mixins/AbstractBlockStateMixin.java
@@ -1,9 +1,6 @@
 package net.xolt.freecam.mixins;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.EntityShapeContext;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -13,15 +10,19 @@ import net.xolt.freecam.config.CollisionWhitelist;
 import net.xolt.freecam.config.ModConfig;
 import net.xolt.freecam.util.FreeCamera;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(AbstractBlock.class)
-public class AbstractBlockMixin {
+@Mixin(AbstractBlock.AbstractBlockState.class)
+public abstract class AbstractBlockStateMixin {
 
-    @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
-    private void onGetCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+    @Shadow
+    public abstract Block getBlock();
+
+    @Inject(method = "getCollisionShape(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/ShapeContext;)Lnet/minecraft/util/shape/VoxelShape;", at = @At("HEAD"), cancellable = true)
+    private void onGetCollisionShape(BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
         if (context instanceof EntityShapeContext && ((EntityShapeContext)context).getEntity() instanceof FreeCamera) {
             // Unless "Always Check Initial Collision" is on and Freecam isn't enabled yet
             if (!ModConfig.INSTANCE.collision.alwaysCheck || Freecam.isEnabled()) {
@@ -31,11 +32,11 @@ public class AbstractBlockMixin {
                 }
             }
             // Ignore transparent block collisions
-            if (ModConfig.INSTANCE.collision.ignoreTransparent && CollisionWhitelist.isTransparent(state.getBlock())) {
+            if (ModConfig.INSTANCE.collision.ignoreTransparent && CollisionWhitelist.isTransparent(getBlock())) {
                 cir.setReturnValue(VoxelShapes.empty());
             }
             // Ignore transparent block collisions
-            if (ModConfig.INSTANCE.collision.ignoreOpenable && CollisionWhitelist.isOpenable(state.getBlock())) {
+            if (ModConfig.INSTANCE.collision.ignoreOpenable && CollisionWhitelist.isOpenable(getBlock())) {
                 cir.setReturnValue(VoxelShapes.empty());
             }
         }
