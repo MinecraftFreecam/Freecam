b2f6429c37d82f8d6ff47efda3f08048402cb34e
hashalite <hashalite@gmail.com>
2023-07-13 15:20:00 -0400

    - Fixed #96 and #99 - Fixed 'Show Player' setting not doing anything

--- src/main/java/net/xolt/freecam/mixins/WorldRendererMixin.java
+++ src/main/java/net/xolt/freecam/mixins/WorldRendererMixin.java
@@ -1,15 +1,13 @@
 package net.xolt.freecam.mixins;
 
-import net.minecraft.client.render.*;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.math.Vec3d;
 import net.xolt.freecam.Freecam;
 import net.xolt.freecam.config.ModConfig;
-import org.joml.Matrix4f;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,20 +17,10 @@ import static net.xolt.freecam.Freecam.MC;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 
-    @Shadow
-    @Final
-    private BufferBuilderStorage bufferBuilders;
-
-    // Makes the player render if showPlayer is enabled.
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;checkEmpty(Lnet/minecraft/client/util/math/MatrixStack;)V", ordinal = 0))
-    private void onRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
-        if (Freecam.isEnabled() && ModConfig.INSTANCE.visual.showPlayer) {
-            Vec3d cameraPos = camera.getPos();
-            renderEntity(MC.player, cameraPos.x, cameraPos.y, cameraPos.z, tickDelta, matrices, bufferBuilders.getEntityVertexConsumers());
+    @Inject(method = "renderEntity", at = @At("HEAD"), cancellable = true)
+    private void onRenderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+        if (entity == MC.player && Freecam.isEnabled() && !ModConfig.INSTANCE.visual.showPlayer) {
+            ci.cancel();
         }
-    }
-
-    @Shadow
-    private void renderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers) {
     }
 }
