151a047f43b6862ba6e4043774c20759680fda7c
tticehurst <46826453+tticehurst@users.noreply.github.com>
2023-12-16 13:11:26 +0000

    Update common/src/main/java/net/xolt/freecam/config/CollisionWhitelist.java
    Co-authored-by: Matt Sturgeon <matt@sturgeon.me.uk>

--- common/src/main/java/net/xolt/freecam/config/CollisionWhitelist.java
+++ common/src/main/java/net/xolt/freecam/config/CollisionWhitelist.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class CollisionWhitelist {
 
     private static final Collection<Class<? extends Block>> transparentWhitelist = List.of(
-            TransparentBlock.class,
+            StainedGlassBlock.class,
+            TintedGlassBlock.class,
             PaneBlock.class,
             BarrierBlock.class);
 
