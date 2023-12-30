a94f0b7f5021fa9a5c176bdf47f87370c349502f
Matt Sturgeon <matt@sturgeon.me.uk>
2023-12-30 15:44:45 +0000

    Show an error when whitelist regex is invalid

--- common/src/main/java/net/xolt/freecam/config/gui/AutoConfigExtensions.java
+++ common/src/main/java/net/xolt/freecam/config/gui/AutoConfigExtensions.java
@@ -9,7 +9,10 @@ import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.network.chat.Component;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 /**
@@ -18,6 +21,7 @@ import java.util.function.Predicate;
  * @see DefaultGuiProviders
  * @see DefaultGuiTransformers
  */
+@SuppressWarnings("JavadocReference")
 public class AutoConfigExtensions {
     static final Component RESET_TEXT = Component.translatable("text.cloth-config.reset_value");
     static final ConfigEntryBuilder ENTRY_BUILDER = ConfigEntryBuilder.create();
@@ -29,11 +33,71 @@ public class AutoConfigExtensions {
         Requirements.apply(registry);
         ModBindingsConfigImpl.apply(registry);
         VariantTooltipImpl.apply(registry);
+        ValidateRegexImpl.apply(registry);
         BoundedContinuousImpl.apply(registry);
         ServerRestrictionDependencies.apply(registry);
     }
 
     static Predicate<Field> isField(Class<?> declaringClass, String... fieldNames) {
         return field -> field.getDeclaringClass().equals(declaringClass) && Arrays.asList(fieldNames).contains(field.getName());
+    }
+
+    static Predicate<Field> isArrayOrListOfType(Type... types) {
+        return field -> {
+            if (field.getType().isArray()) {
+                Class<?> component = field.getType().getComponentType();
+                return Arrays.asList(types).contains(component);
+            }
+            return isListOfType(types).test(field);
+        };
+    }
+
+    static Predicate<Field> isNotArrayOrListOfType(Type... types) {
+        return field -> {
+            if (field.getType().isArray()) {
+                Class<?> component = field.getType().getComponentType();
+                return Arrays.stream(types).noneMatch(component::equals);
+            }
+            return isNotListOfType(types).test(field);
+        };
+    }
+
+    /**
+     * Returns a predicate that tests if the field is a {@link List} of a particular {@link Type}, e.g. a {@code List<Integer>}.
+     * <p>
+     * Based on a {@link DefaultGuiProviders#isListOfType(Type...) helper method} in cloth-config.
+     *
+     * @param types the types to check for in the list's parameter
+     * @return {@code true} if the field is a list containing the provided type, {@code false} otherwise
+     */
+    static Predicate<Field> isListOfType(Type... types) {
+        return field -> {
+            if (List.class.isAssignableFrom(field.getType()) && field.getGenericType() instanceof ParameterizedType generic) {
+                Type[] args = generic.getActualTypeArguments();
+                return args.length == 1 && Arrays.asList(types).contains(args[0]);
+            } else {
+                return false;
+            }
+        };
+    }
+
+    /**
+     * Returns a predicate that tests if the field is a {@link List} <strong>not</strong> of a particular {@link Type},
+     * e.g. any {@code List} that isn't a {@code List<Integer>}.
+     * <p>
+     * Based on a {@link DefaultGuiProviders#isNotListOfType(Type...) helper method} in cloth-config.
+     *
+     * @param types the types to check for in the list's parameter
+     * @return {@code true} if the field is a list not containing the provided type, {@code false} otherwise
+     */
+    static Predicate<Field> isNotListOfType(Type... types) {
+        return field -> {
+            if (List.class.isAssignableFrom(field.getType()) && field.getGenericType() instanceof ParameterizedType generic) {
+                Type[] args = generic.getActualTypeArguments();
+                return args.length == 1 && Arrays.stream(types).noneMatch(args[0]::equals);
+            } else {
+                return false;
+            }
+        };
     }
 }
