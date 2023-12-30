6b41e84f6e25c26d6f65c794867c1a9d006502cf
Matt Sturgeon <matt@sturgeon.me.uk>
2023-12-30 16:31:10 +0000

    Refactor config gui extensions

---
+++ common/src/main/java/net/xolt/freecam/config/gui/AutoConfigExtensions.java
@@ -0,0 +1,25 @@
+package net.xolt.freecam.config.gui;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.gui.DefaultGuiProviders;
+import me.shedaniel.autoconfig.gui.DefaultGuiTransformers;
+import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+
+/**
+ * Extensions and modifications to AutoConfig.
+ *
+ * @see DefaultGuiProviders
+ * @see DefaultGuiTransformers
+ */
+public class AutoConfigExtensions {
+    static final ConfigEntryBuilder ENTRY_BUILDER = ConfigEntryBuilder.create();
+
+    private AutoConfigExtensions() {}
+
+    public static void apply(Class<? extends ConfigData> configClass) {
+        GuiRegistry registry = AutoConfig.getGuiRegistry(configClass);
+        VariantTooltipImpl.apply(registry);
+    }
+}
