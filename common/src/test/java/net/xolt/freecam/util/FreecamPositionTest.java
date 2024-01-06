960e2952732f399342e906a2b8fd79f05bb35b98
Matt Sturgeon <matt@sturgeon.me.uk>
2024-01-06 20:27:40 +0000

    Stub FreecamPosition test

---
+++ common/src/test/java/net/xolt/freecam/util/FreecamPositionTest.java
@@ -0,0 +1,38 @@
+package net.xolt.freecam.util;
+
+import net.xolt.freecam.test.extension.BootstrapMinecraft;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@BootstrapMinecraft
+class FreecamPositionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setRotation() {
+    }
+
+    @Test
+    void mirrorRotation() {
+    }
+
+    @Test
+    void moveForward() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getChunkPos() {
+    }
+}
