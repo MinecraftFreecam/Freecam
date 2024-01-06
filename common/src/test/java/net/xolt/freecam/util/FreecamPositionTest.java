dd36db94f7febaaff3bc9ea8caa6a59df726be2c
Matt Sturgeon <matt@sturgeon.me.uk>
2024-01-06 21:48:22 +0000

    Continue stubbing initial test

--- common/src/test/java/net/xolt/freecam/util/FreecamPositionTest.java
+++ common/src/test/java/net/xolt/freecam/util/FreecamPositionTest.java
@@ -1,15 +1,25 @@
 package net.xolt.freecam.util;
 
+import net.minecraft.client.player.RemotePlayer;
 import net.xolt.freecam.test.extension.BootstrapMinecraft;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @BootstrapMinecraft
+@ExtendWith(MockitoExtension.class)
 class FreecamPositionTest {
+
+    @Mock RemotePlayer player;
+    private FreecamPosition position;
 
     @BeforeEach
     void setUp() {
+        position = new FreecamPosition(player);
     }
 
     @AfterEach
@@ -18,6 +28,10 @@ class FreecamPositionTest {
 
     @Test
     void setRotation() {
+        position.setRotation(10, 20);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(10, position.yaw),
+                () -> Assertions.assertEquals(20, position.pitch));
     }
 
     @Test
