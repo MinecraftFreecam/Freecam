f45294202551ef35668a3cc233bd209e9239d195
Matt Sturgeon <matt@sturgeon.me.uk>
2024-01-12 16:54:34 +0000

    Test quaternion is updated by setRotation

--- common/src/test/java/net/xolt/freecam/util/FreecamPositionTest.java
+++ common/src/test/java/net/xolt/freecam/util/FreecamPositionTest.java
@@ -2,6 +2,7 @@ package net.xolt.freecam.util;
 
 import net.minecraft.client.player.RemotePlayer;
 import net.xolt.freecam.test.extension.BootstrapMinecraft;
+import org.joml.Quaternionf;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +13,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static net.xolt.freecam.test.util.TestUtils.getFieldValue;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @BootstrapMinecraft
@@ -51,6 +53,40 @@ class FreecamPositionTest {
                     );
                 }
         );
+    }
+
+    @Test
+    @DisplayName("setRotation correctly updates quaternion")
+    void setRotation_Quaternion() {
+        Quaternionf initialRotation = getFieldValue(Quaternionf.class, position, "rotation");
+        assertThat(initialRotation).as("Initially all zeros").satisfies(
+                rotation -> assertThat(rotation.x).as("x is correct").isEqualTo(0),
+                rotation -> assertThat(rotation.y).as("y is correct").isEqualTo(0),
+                rotation -> assertThat(rotation.z).as("z is correct").isEqualTo(0),
+                rotation -> assertThat(rotation.w).as("w is correct").isEqualTo(1)
+        );
+        position.setRotation(10, 20);
+        Quaternionf rotationAfterTenTwenty = getFieldValue(Quaternionf.class, position, "rotation");
+        assertThat(rotationAfterTenTwenty).as("Mutated correctly").satisfies(
+                // Magic numbers obtained from working implementation
+                rotation -> assertThat(rotation.x).as("x is correct").isEqualTo(0.1729874f),
+                rotation -> assertThat(rotation.y).as("y is correct").isEqualTo(-0.08583164f),
+                rotation -> assertThat(rotation.z).as("z is correct").isEqualTo(0.015134435f),
+                rotation -> assertThat(rotation.w).as("w is correct").isEqualTo(0.98106027f)
+        );
+        position.setRotation(100, 200);
+        Quaternionf rotationAfterHundredTwoHundred = getFieldValue(Quaternionf.class, position, "rotation");
+        assertThat(rotationAfterHundredTwoHundred).as("Mutated correctly").satisfies(
+                // Magic numbers obtained from working implementation
+                rotation -> assertThat(rotation.x).as("x is correct").isEqualTo(0.63302225f),
+                rotation -> assertThat(rotation.y).as("y is correct").isEqualTo(0.13302235f),
+                rotation -> assertThat(rotation.z).as("z is correct").isEqualTo(0.7544065f),
+                rotation -> assertThat(rotation.w).as("w is correct").isEqualTo(-0.11161902f)
+        );
+        position.setRotation(0, 0);
+        Quaternionf rotationReset = getFieldValue(Quaternionf.class, position, "rotation");
+        assertThat(rotationReset).as("Reset matches initial value").isEqualTo(initialRotation);
+
     }
 
     @Test
