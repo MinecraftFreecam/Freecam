MinecraftFreecam/Freecam

commit 01143ecd54ea46480977154c525b77972de0546b

Refactor tick handlers
Extract the `ClientTickEvent.CLIENT_POST` handler into its own method `postTick` and clean up the code a little.

Replace preTick mixin with a `ClientTickEvent` listener, which uses Fabric's `START_CLIENT_TICK`[1] and Forge's `ClientTickEvent` with `Phase.START`[2].
This is functionally equivalent.

[1]: https://maven.fabricmc.net/docs/fabric-api-0.91.1+1.20.2/net/fabricmc/fabric/api/client/event/lifecycle/v1/ClientTickEvents.html#START_CLIENT_TICK
[2]: https://nekoyue.github.io/ForgeJavaDocs-NG/javadoc/1.19.3/net/minecraftforge/event/TickEvent.ClientTickEvent.html#%3Cinit%3E(net.minecraftforge.event.TickEvent.Phase)

common/src/main/java/net/xolt/freecam/Freecam.java
@@ -47,37 +47,58 @@ public class Freecam {
     public static void init() {
         ModConfig.init();
         Stream.of(KEY_TOGGLE, KEY_PLAYER_CONTROL, KEY_TRIPOD_RESET, KEY_CONFIG_GUI).forEach(KeyMappingRegistry::register);
+        ClientTickEvent.CLIENT_PRE.register(Freecam::preTick);
+        ClientTickEvent.CLIENT_POST.register(Freecam::postTick);
+    }
 
-        ClientTickEvent.CLIENT_POST.register(client -> {
-            if (KEY_TRIPOD_RESET.isPressed()) {
-                for (KeyBinding hotbarKey : MC.options.hotbarKeys) {
-                    while (hotbarKey.wasPressed()) {
-                        resetCamera(hotbarKey.getDefaultKey().getCode());
-                        while (KEY_TRIPOD_RESET.wasPressed()) {}
-                    }
-                }
-            }
-
-            if (KEY_TOGGLE.isPressed()) {
-                for (KeyBinding hotbarKey : MC.options.hotbarKeys) {
-                    while (hotbarKey.wasPressed()) {
-                        toggleTripod(hotbarKey.getDefaultKey().getCode());
-                        while (KEY_TOGGLE.wasPressed()) {}
-                    }
-                }
-            } else if (KEY_TOGGLE.wasPressed()) {
+    private static void preTick(MinecraftClient mc) {
+        if (isEnabled()) {
+            // Disable if the previous tick asked us to
+            if (disableNextTick()) {
                 toggle();
-                while (KEY_TOGGLE.wasPressed()) {}
+                disableNextTick = false;
             }
 
-            while (KEY_PLAYER_CONTROL.wasPressed()) {
-                switchControls();
+            // Prevent player from being controlled when freecam is enabled
+            if (mc.player != null && mc.player.input instanceof KeyboardInput && !isPlayerControlEnabled()) {
+                Input input = new Input();
+                input.sneaking = mc.player.input.sneaking; // Makes player continue to sneak after freecam is enabled.
+                mc.player.input = input;
             }
 
-            while (KEY_CONFIG_GUI.wasPressed()) {
-                MC.setScreen(AutoConfig.getConfigScreen(ModConfig.class, MC.currentScreen).get());
+            mc.gameRenderer.setRenderHand(ModConfig.INSTANCE.visual.showHand);
+        }
+    }
+
+    private static void postTick(MinecraftClient mc) {
+        if (KEY_TRIPOD_RESET.isPressed()) {
+            for (KeyBinding hotbarKey : mc.options.hotbarKeys) {
+                while (hotbarKey.wasPressed()) {
+                    resetCamera(hotbarKey.getDefaultKey().getCode());
+                    while (KEY_TRIPOD_RESET.wasPressed()) {}
+                }
             }
-        });
+        }
+
+        if (KEY_TOGGLE.isPressed()) {
+            for (KeyBinding hotbarKey : mc.options.hotbarKeys) {
+                while (hotbarKey.wasPressed()) {
+                    toggleTripod(hotbarKey.getDefaultKey().getCode());
+                    while (KEY_TOGGLE.wasPressed()) {}
+                }
+            }
+        } else if (KEY_TOGGLE.wasPressed()) {
+            toggle();
+            while (KEY_TOGGLE.wasPressed()) {}
+        }
+
+        while (KEY_PLAYER_CONTROL.wasPressed()) {
+            switchControls();
+        }
+
+        while (KEY_CONFIG_GUI.wasPressed()) {
+            mc.setScreen(AutoConfig.getConfigScreen(ModConfig.class, mc.currentScreen).get());
+        }
     }
 
     public static void onDisconnect() {

common/src/main/java/net/xolt/freecam/mixins/MinecraftClientMixin.java
@@ -1,8 +1,6 @@
 package net.xolt.freecam.mixins;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.input.Input;
-import net.minecraft.client.input.KeyboardInput;
 import net.xolt.freecam.Freecam;
 import net.xolt.freecam.config.ModConfig;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,28 +9,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static net.xolt.freecam.Freecam.MC;
-
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-
-    // Prevents player from being controlled when freecam is enabled.
-    @Inject(method = "tick", at = @At("HEAD"))
-    private void onTick(CallbackInfo ci) {
-        if (Freecam.isEnabled()) {
-            if (MC.player != null && MC.player.input instanceof KeyboardInput && !Freecam.isPlayerControlEnabled()) {
-                Input input = new Input();
-                input.sneaking = MC.player.input.sneaking; // Makes player continue to sneak after freecam is enabled.
-                MC.player.input = input;
-            }
-            MC.gameRenderer.setRenderHand(ModConfig.INSTANCE.visual.showHand);
-
-            if (Freecam.disableNextTick()) {
-                Freecam.toggle();
-                Freecam.setDisableNextTick(false);
-            }
-        }
-    }
 
     // Prevents attacks when allowInteract is disabled.
     @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)

commit f8f9cbd4d3fafedfd2d56d66fae41efbc1919bb4

fix: disableNextTick prevents player walking
Regression introduced in 01143ecd54ea46480977154c525b77972de0546b
because the `disableNextTick` check was moved before setting `mc.player.input`.

Fixes #155

common/src/main/java/net/xolt/freecam/Freecam.java
@@ -37,12 +37,13 @@ public class Freecam {
     private static CameraType rememberedF5 = null;
 
     public static void preTick(Minecraft mc) {
-        if (isEnabled()) {
-            // Disable if the previous tick asked us to
-            if (disableNextTick) {
-                toggle();
-            }
+        // Disable if the previous tick asked us to
+        if (disableNextTick && isEnabled()) {
+            toggle();
+        }
+        disableNextTick = false;
 
+        if (isEnabled()) {
             // Prevent player from being controlled when freecam is enabled
             if (mc.player != null && mc.player.input instanceof KeyboardInput && !isPlayerControlEnabled()) {
                 Input input = new Input();
@@ -52,7 +53,6 @@ public class Freecam {
 
             mc.gameRenderer.setRenderHand(ModConfig.INSTANCE.visual.showHand);
         }
-        disableNextTick = false;
     }
 
     public static void postTick(Minecraft mc) {