21cbfd4ec44e309fa415628d10460989c355e9a0
Matt Sturgeon <matt@sturgeon.me.uk>
2024-04-20 04:34:25 +0100

    Fix mixins not applying

--- common/src/main/java/net/xolt/freecam/mixins/LevelRendererMixin.java
+++ common/src/main/java/net/xolt/freecam/mixins/LevelRendererMixin.java
@@ -3,11 +3,15 @@ package net.xolt.freecam.mixins;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.TickRateManager;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
 import net.xolt.freecam.Freecam;
 import net.xolt.freecam.config.ModConfig;
 import org.joml.Matrix4f;
+import org.joml.Matrix4fStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,20 +20,43 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static net.xolt.freecam.Freecam.MC;
+import static org.spongepowered.asm.mixin.injection.callback.LocalCapture.CAPTURE_FAILHARD;
 
 @Mixin(LevelRenderer.class)
-public class LevelRendererMixin {
+public abstract class LevelRendererMixin {
 
     @Shadow @Final private RenderBuffers renderBuffers;
 
     @Shadow private void renderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, PoseStack matrices, MultiBufferSource vertexConsumers) {}
 
     // Makes the player render if showPlayer is enabled.
-    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;checkPoseStack(Lcom/mojang/blaze3d/vertex/PoseStack;)V", ordinal = 0))
-    private void onRender(PoseStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
+    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;checkPoseStack(Lcom/mojang/blaze3d/vertex/PoseStack;)V", ordinal = 0), locals = CAPTURE_FAILHARD)
+    private void onRender(float partialTick,
+                          long nanoTime,
+                          boolean renderBlockOutline,
+                          Camera camera,
+                          GameRenderer gameRenderer,
+                          LightTexture lightTexture,
+                          Matrix4f matrix4f,
+                          Matrix4f matrix4f2,
+                          CallbackInfo ci,
+                          TickRateManager tickRateManager,
+                          float g,
+                          ProfilerFiller profilerFiller,
+                          Vec3 cameraPosition,
+                          double x,
+                          double y,
+                          double z,
+                          boolean frustumNotNull,
+                          Frustum frustum,
+                          float renderDistance,
+                          boolean fog,
+                          Matrix4fStack modelViewStack,
+                          boolean bl4,
+                          PoseStack poseStack,
+                          MultiBufferSource.BufferSource bufferSource) {
         if (Freecam.isEnabled() && ModConfig.INSTANCE.visual.showPlayer) {
-            Vec3 cameraPos = camera.getPosition();
-            renderEntity(MC.player, cameraPos.x, cameraPos.y, cameraPos.z, tickDelta, matrices, renderBuffers.bufferSource());
+            renderEntity(MC.player, x, y, z, partialTick, poseStack, renderBuffers.bufferSource());
         }
     }
 }
