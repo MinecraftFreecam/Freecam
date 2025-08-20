d21bca120b2354be75febaba0ffc683e0416d389
Benjamin Mayes <czahrien@gmail.com>
2025-08-20 19:45:34 -0400

    fix Full Brightness black UI textures on MacOS (#299)

--- common/src/main/java/net/xolt/freecam/mixins/LightTextureMixin.java
+++ common/src/main/java/net/xolt/freecam/mixins/LightTextureMixin.java
@@ -3,6 +3,7 @@ package net.xolt.freecam.mixins;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.world.level.dimension.DimensionType;
 import net.xolt.freecam.Freecam;
 import net.xolt.freecam.config.ModConfig;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,10 +12,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(LightTexture.class)
 public class LightTextureMixin {
 
-    @WrapOperation(method = "updateLightTexture", at = @At(value = "INVOKE", target = "Ljava/lang/Double;floatValue()F", ordinal = 1))
-    private float onSetBrightnessFactor(Double instance, Operation<Float> original) {
+    @WrapOperation(method = "updateLightTexture",
+            at = @At(value="INVOKE", target="Lnet/minecraft/world/level/dimension/DimensionType;ambientLight()F"))
+    private float onSetBrightnessFactor(DimensionType instance, Operation<Float> original) {
         if (Freecam.isEnabled() && ModConfig.INSTANCE.visual.fullBright) {
-            return Float.MAX_VALUE;
+            return 1.0f;
         }
         return original.call(instance);
     }
