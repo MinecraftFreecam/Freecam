a208f352d3929eb4932396d54d9149b35dce5022
hashalite <hashalite@gmail.com>
2025-10-11 20:09:11 -0400

    Avoid using MixinExtras for full brightness

--- common/src/main/java/net/xolt/freecam/mixins/LightTextureMixin.java
+++ common/src/main/java/net/xolt/freecam/mixins/LightTextureMixin.java
@@ -1,23 +1,21 @@
 package net.xolt.freecam.mixins;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.xolt.freecam.Freecam;
 import net.xolt.freecam.config.ModConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LightTexture.class)
 public class LightTextureMixin {
 
-    @WrapOperation(method = "updateLightTexture",
-            at = @At(value="INVOKE", target="Lnet/minecraft/client/renderer/LightTexture;getBrightness(Lnet/minecraft/world/level/dimension/DimensionType;I)F"))
-    private float onSetBrightnessFactor(DimensionType dimensionType, int lightLevel, Operation<Float> original) {
+    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
+    private static void onGetBrightness(DimensionType dimensionType, int lightLevel, CallbackInfoReturnable<Float> cir) {
         if (Freecam.isEnabled() && ModConfig.INSTANCE.visual.fullBright) {
-            return 1.0f;
+            cir.setReturnValue(1.0f);
         }
-        return original.call(dimensionType, lightLevel);
     }
 }
