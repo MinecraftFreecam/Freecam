513d7076d4a87c391af8abf79428da4e3f9f9cff
Matt Sturgeon <matt@sturgeon.me.uk>
2025-12-28 13:16:24 +0000

    build: make version catalog available to buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,3 +13,9 @@ repositories {
 tasks.test {
     useJUnitPlatform()
 }
+
+/**
+ * Transforms a Version Catalog's Gradle Plugin alias into normal dependency notation.
+ */
+fun DependencyHandlerScope.plugin(plugin: Provider<PluginDependency>) =
+    plugin.map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" }
