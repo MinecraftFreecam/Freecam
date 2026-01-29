62bc1c17dcdb282acda164f25a4b8d33bff997e3
Matt Sturgeon <matt@sturgeon.me.uk>
2026-01-29 18:51:12 +0000

    build: fix versionless gradle config
    Move "true root project" configuration from `build.gradle.kts` to
    `stonecutter.gradle.kts`.
    
    `build.gradle.kts` is evaluated by stonecutter for each versioned
    rootProject (e.g. `:1.21.1x`).
    
    `stonecutter.gradle.kts` is evaluated by the _actual_ rootProject (`:`).

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,6 +1,15 @@
+import net.xolt.freecam.gradle.BumpVersionTask
+import org.jetbrains.changelog.tasks.BaseChangelogTask
+
+// This file represents the version-less `rootProject` (:)
+// Version-agnostic tasks should go here, as well as configuration for stonecutter itself
+
 plugins {
     id("dev.kikugie.stonecutter")
+    id("org.jetbrains.changelog")
 }
+
+stonecutter active "1.21.11"
 
 stonecutter {
     parameters {
@@ -34,4 +43,48 @@ stonecutter {
     }
 }
 
-stonecutter active "1.21.11"
+tasks.named<Wrapper>("wrapper") {
+    // Use "all" so we get sources and javadoc too
+    distributionType = Wrapper.DistributionType.ALL
+    gradleVersion = "9.2.1"
+}
+
+// Helper task that bumps the version number
+tasks.register<BumpVersionTask>("bumpVersion") {
+    group = "version"
+    input = file("gradle.properties")
+    key = "mod.version"
+}
+
+// Move the changelog tasks to the "version" group
+tasks.withType<BaseChangelogTask>().configureEach {
+    group = "version"
+}
+
+changelog {
+    // Use the mod_version in gradle.properties as the release version/tag.
+    // Build tag/diff links using the github repo URL.
+    version = property("mod.version") as String
+    repositoryUrl = property("mod.source") as String
+
+    // Title & intro are printed right at the start of the changelog.
+    // The values here will replace whatever exists in the file when patching.
+    title = "Changelog"
+    introduction = """
+    All notable changes to this project will be documented in this file.
+    
+    This file is formatted as per [Keep a Changelog](https://keepachangelog.com/en/1.0.0),
+    and Freecam's versioning is based on [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+    """.trimIndent()
+
+    // Group sub-headings added to the "unreleased" section when patching the changelog.
+    // Other sub-headings can still be added manually, if required.
+    groups = listOf("Added",
+        "Changed",
+        "Removed",
+        "Fixed")
+
+    // Regex used to find versions in headings.
+    // The default regex only supports semantic versions, this one is more lenient.
+    headerParserRegex = Regex("(\\d+(?:\\.\\d+)+(?:-[-a-z]+\\d*)?)")
+}
