4e1e8ec8e17cc363709176ebc8d09cb9e2a5ea4c
Matt Sturgeon <matt@sturgeon.me.uk>
2026-02-01 13:28:26 +0000

    Make ModMetadata an interface

--- build-logic/api/src/main/kotlin/net/xolt/freecam/model/ModMetadata.kt
+++ build-logic/api/src/main/kotlin/net/xolt/freecam/model/ModMetadata.kt
@@ -3,33 +3,53 @@ package net.xolt.freecam.model
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+interface ModMetadata {
+    val id: String
+    val name: String
+    val group: String
+    val version: String
+    val releaseType: ReleaseType
+    val authors: List<String>
+    val description: String
+    val license: String
+    val homepageUrl: UrlString
+    val sourceUrl: UrlString
+    val issuesUrl: UrlString
+    val githubReleasesUrl: UrlString
+    val curseforgeUrl: UrlString
+    val curseforgeId: String
+    val modrinthUrl: UrlString
+    val modrinthId: String
+    val crowdinUrl: UrlString
+}
+
 @Serializable
-data class ModMetadata(
-    val id: String,
-    val name: String,
-    val group: String,
-    val version: String,
+data class ModMetadataToml(
+    override val id: String,
+    override val name: String,
+    override val group: String,
+    override val version: String,
     @SerialName("release_type")
-    val releaseType: ReleaseType,
-    val authors: List<String>,
-    val description: String,
-    val license: String,
+    override val releaseType: ReleaseType,
+    override val authors: List<String>,
+    override val description: String,
+    override val license: String,
     @SerialName("homepage")
-    val homepageUrl: UrlString,
+    override val homepageUrl: UrlString,
     @SerialName("source")
-    val sourceUrl: UrlString,
+    override val sourceUrl: UrlString,
     @SerialName("issues")
-    val issuesUrl: UrlString,
+    override val issuesUrl: UrlString,
     @SerialName("github_releases")
-    val githubReleasesUrl: UrlString,
+    override val githubReleasesUrl: UrlString,
     @SerialName("curseforge")
-    val curseforgeUrl: UrlString,
+    override val curseforgeUrl: UrlString,
     @SerialName("curseforge_id")
-    val curseforgeId: String,
+    override val curseforgeId: String,
     @SerialName("modrinth")
-    val modrinthUrl: UrlString,
+    override val modrinthUrl: UrlString,
     @SerialName("modrinth_id")
-    val modrinthId: String,
+    override val modrinthId: String,
     @SerialName("crowdin")
-    val crowdinUrl: UrlString,
-)
+    override val crowdinUrl: UrlString,
+) : ModMetadata
--- build-logic/settings/src/main/kotlin/net/xolt/freecam/gradle/ModMetadataSettingsPlugin.kt
+++ build-logic/settings/src/main/kotlin/net/xolt/freecam/gradle/ModMetadataSettingsPlugin.kt
@@ -4,13 +4,14 @@ import dev.eav.tomlkt.Toml
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import net.xolt.freecam.model.ModMetadata
+import net.xolt.freecam.model.ModMetadataToml
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 import org.gradle.kotlin.dsl.add
 
 @Serializable
-private data class Metadata(
-    val mod: ModMetadata,
+private data class MetadataToml(
+    val mod: ModMetadataToml,
 )
 
 class ModMetadataSettingsPlugin : Plugin<Settings> {
@@ -18,7 +19,7 @@ class ModMetadataSettingsPlugin : Plugin<Settings> {
         val name = "meta"
         val file = settings.rootDir.resolve("metadata.toml")
         val toml = file.readText()
-        val metadata = Toml.decodeFromString<Metadata>(toml)
+        val metadata = Toml.decodeFromString<MetadataToml>(toml)
 
         settings.extensions.add<ModMetadata>(name, metadata.mod)
         settings.gradle.settingsEvaluated {
