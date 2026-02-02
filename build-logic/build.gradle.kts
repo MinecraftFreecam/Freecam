5e4d61345129b38e8454888eb76d736d49e5d552
Matt Sturgeon <matt@sturgeon.me.uk>
2026-02-02 22:15:48 +0000

    ci(build): add `:build-logic:check`
    Aggregates build-logic test tasks.

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,14 @@
+
+val check by tasks.registering {
+    aggregateByName()
+}
+
+val test by tasks.registering {
+    aggregateByName()
+}
+
+fun Task.aggregateByName() {
+    dependsOn(provider {
+        subprojects.mapNotNull { it.tasks.findByName(name) }
+    })
+}
