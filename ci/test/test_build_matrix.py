3b57d1c2657ef963b6531e3b06cceeefb90e25aa
Matt Sturgeon <matt@sturgeon.me.uk>
2026-03-14 19:54:07 +0000

    ci(build_matrix): store static matrix jobs as TOML
    Introduce `ci/matrix_jobs.toml` for defining static matrix jobs,
    like :build-logic tests.

--- ci/test/test_build_matrix.py
+++ ci/test/test_build_matrix.py
@@ -1,4 +1,10 @@
-from freecam_ci.build_matrix import build_matrix, MatrixJob
+from pathlib import Path
+
+import pytest
+
+from freecam_ci.build_matrix import build_version_matrix, load_matrix_jobs, MatrixJob
+
+FIXTURES = Path(__file__).parent / "fixtures"
 
 
 def test_matrixjob_to_dict():
@@ -17,7 +23,44 @@ def test_matrixjob_to_dict():
     assert "upload_days" not in d
 
 
-def test_build_matrix_basic():
+def test_matrixjob_from_dict():
+    value = {
+        "name": "Build test",
+        "gradle_args": [":common:test"],
+        "upload_path": "build/libs/*.jar",
+        "upload_name": "artifact",
+    }
+    job = MatrixJob.from_dict(value)
+    assert job == MatrixJob(
+        name="Build test",
+        gradle_args=[":common:test"],
+        upload_name="artifact",
+        upload_path="build/libs/*.jar",
+        upload_days=None,
+    )
+
+
+def test_matrixjob_from_dict_upload_pair_required():
+    value = {
+        "name": "Build test",
+        "gradle_args": [":common:test"],
+        "upload_path": "build/libs/*.jar",
+    }
+    with pytest.raises(ValueError):
+        MatrixJob.from_dict(value)
+
+
+def test_matrixjob_from_dict_upload_days_requires_upload():
+    value = {
+        "name": "Build test",
+        "gradle_args": [":common:test"],
+        "upload_days": 5,
+    }
+    with pytest.raises(ValueError):
+        MatrixJob.from_dict(value)
+
+
+def test_build_version_matrix_basic():
     data = {
         "$schema": "123456",
         "versions": {
@@ -35,12 +78,11 @@ def test_build_matrix_basic():
         },
     }
     version = "1.2.3"
-    matrix = build_matrix(version, data)
-    assert len(matrix) == 5
+    matrix = build_version_matrix(version, data)
+    assert len(matrix) == 4
     names = [job.name for job in matrix]
     assert "Build 1.20" in names
     assert "Build 1.21" in names
-    assert "Build logic tests" in names
 
     # Check gradle_args
     job_120 = next(job for job in matrix if job.name == "Build 1.20")
@@ -62,3 +104,24 @@ def test_build_matrix_basic():
     assert job_118.upload_name == "freecam-1.2.3-1.18"
     assert job_118.upload_path == "build/libs/1.2.3/*.jar"
     assert all("common" not in arg for arg in job_118.gradle_args)
+
+
+def test_load_matrix_jobs_empty():
+    matrix = load_matrix_jobs(matrix_jobs_file=FIXTURES / "empty_matrix_jobs.toml")
+    assert matrix == []
+
+
+def test_load_matrix_jobs():
+    matrix = load_matrix_jobs(matrix_jobs_file=FIXTURES / "matrix_jobs.toml")
+    assert len(matrix) == 1
+
+    assert matrix[0].name == "test job 1"
+    assert matrix[0].gradle_args == ["a", "b", "c"]
+    assert matrix[0].upload_name is None
+    assert matrix[0].upload_path is None
+    assert matrix[0].upload_days is None
+
+
+def test_load_matrix_jobs_invalid():
+    with pytest.raises(ValueError):
+        load_matrix_jobs(matrix_jobs_file=FIXTURES / "bad_matrix_jobs.toml")
