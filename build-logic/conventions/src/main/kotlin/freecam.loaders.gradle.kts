02823dfb6e8c5c89956e04bd7017af2e63ab03dd
Matt Sturgeon <matt@sturgeon.me.uk>
2026-03-26 06:34:34 +0000

    revert "build: depend on `:common` properly"
    This partially reverts commit 7670681ed67f30e847576a750ef1f53378c357a4
    
    While depending on compiled classes is nice in theory, we are running
    into unexpected issues with ABI compatibility between vanilla and
    forge-patched Minecraft.

--- build-logic/conventions/src/main/kotlin/freecam.loaders.gradle.kts
+++ build-logic/conventions/src/main/kotlin/freecam.loaders.gradle.kts
@@ -4,19 +4,18 @@ plugins {
     id("freecam.common")
 }
 
-dependencies {
-    implementation(project(path = commonNode.project.path, configuration = "namedElements"))
+sourceSets {
+    main {
+        // Manually depend on common's pre-processed sources.
+        // NOTE: loaders have no build dependency on common, so API/implementation
+        //  classes and transitive dependencies must be manually propagated.
+        val commonTasks = commonNode.project.tasks
+        java.srcDirs(commonTasks.named<Sync>("stonecutterGenerate").map { it.destinationDir })
+        resources.srcDirs(commonTasks.processResources.map { it.destinationDir })
+    }
 }
 
 tasks {
-    processResources {
-        from(commonNode.project.tasks.processResources)
-    }
-
-    jar {
-        from(commonNode.project.tasks.compileJava)
-    }
-
     register<ProjectReleaseMetadataTask>("generateReleaseMetadata") {
         group = "publishing"
         description = "Generates release metadata for publishing"
