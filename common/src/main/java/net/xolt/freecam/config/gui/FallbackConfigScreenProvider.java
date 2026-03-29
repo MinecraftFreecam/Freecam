4020cc65f7a4c81a53b91b4937077f65706242e9
Matt Sturgeon <matt@sturgeon.me.uk>
2026-03-29 19:21:05 +0100

    refactor(config): expose loaded providers to fallback

--- common/src/main/java/net/xolt/freecam/config/gui/FallbackConfigScreenProvider.java
+++ common/src/main/java/net/xolt/freecam/config/gui/FallbackConfigScreenProvider.java
@@ -2,11 +2,19 @@ package net.xolt.freecam.config.gui;
 
 import net.minecraft.client.gui.screens.Screen;
 
+import java.util.Collection;
+
 final class FallbackConfigScreenProvider implements ConfigScreenProvider {
+
+    private final Collection<OptionalProvider> supportedProviders;
+
+    public FallbackConfigScreenProvider(Collection<OptionalProvider> supportedProviders) {
+        this.supportedProviders = supportedProviders;
+    }
 
     @Override
     public Screen getConfigScreen(Screen parent) {
-        return new FallbackConfigScreen(parent);
+        return new FallbackConfigScreen(parent, supportedProviders);
     }
 
     @Override
