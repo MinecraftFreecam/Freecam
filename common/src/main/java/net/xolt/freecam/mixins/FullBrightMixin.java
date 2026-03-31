3d3e99166b060dfda44b0e6001c810084705b21c
Matt Sturgeon <matt@sturgeon.me.uk>
2026-03-31 01:50:32 +0100

    fix: fullbright

--- common/src/main/java/net/xolt/freecam/mixins/FullBrightMixin.java
+++ common/src/main/java/net/xolt/freecam/mixins/FullBrightMixin.java
@@ -1,27 +1,39 @@
 package net.xolt.freecam.mixins;
 
-//~ if >=26.0 LightTexture -> Lightmap {
+//~ if >=26.0 LightTexture -> Lightmap
 import net.minecraft.client.renderer.LightTexture;
 import net.xolt.freecam.Freecam;
 import net.xolt.freecam.config.ModConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-//? if <26.1 && >=1.21.11 {
+//? if >=1.21.11 {
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 //? } else {
 /*import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 *///? }
-//~ if >=1.19 'net.minecraft.world.level.Level' -> 'net.minecraft.world.level.dimension.DimensionType'
+//? if >=26.1 {
+/*import net.minecraft.client.renderer.state.LightmapRenderState;
+*///? } else if >=1.19 {
 import net.minecraft.world.level.dimension.DimensionType;
+//? } else {
+/*import net.minecraft.world.level.Level;
+*///? }
 
+//~ if >=26.0 LightTexture -> Lightmap
 @Mixin(LightTexture.class)
 public class FullBrightMixin {
 
-    // FIXME: This impl didn't work on 1.21.11 - it probably won't on 26.1
-    // Even though the hook still applies, actual brightness is not increased
-    //? if <26.1 && >=1.21.11 {
+    //? if >=26.1 {
+    /*@WrapOperation(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/state/LightmapRenderState;brightness:F"))
+    private float getBrightness(LightmapRenderState instance, Operation<Float> original) {
+        if (Freecam.isEnabled() && ModConfig.get().isFullBrightEnabled()) {
+            return 16.0f;
+        }
+        return original.call(instance);
+    }
+    *///? } else if >=1.21.11 {
     @WrapOperation(method = "updateLightTexture", at = @At(value="INVOKE", target="Lnet/minecraft/world/level/dimension/DimensionType;ambientLight()F"))
     private float getBrightness(DimensionType instance, Operation<Float> original) {
         if (Freecam.isEnabled() && ModConfig.get().isFullBrightEnabled()) {
@@ -40,4 +52,3 @@ public class FullBrightMixin {
     }
     *///? }
 }
-//~ }
