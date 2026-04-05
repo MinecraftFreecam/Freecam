60c877d5e5292a86a54164a4baba99ee663c06a1
Matt Sturgeon <matt@sturgeon.me.uk>
2026-04-05 09:35:53 +0100

    ci(build_matrix): support `archive: false` artifact uploads
    GitHub Actions artifacts recently added non-archive support, enable it
    in our build matrix model.

--- ci/src/freecam_ci/matrix_model.py
+++ ci/src/freecam_ci/matrix_model.py
@@ -11,6 +11,7 @@ class MatrixJob:
     upload_name: str | None = None
     upload_path: str | None = None
     upload_days: int | None = None
+    upload_archive: bool | None = None
 
     def to_dict(self) -> dict:
         """Convert to dict, omitting None values."""
@@ -18,13 +19,43 @@ class MatrixJob:
 
     @classmethod
     def from_dict(cls, value: dict[str, Any]):
+        upload_name = value.get("upload_name")
+        upload_path = value.get("upload_path")
+        upload_days = value.get("upload_days")
+        upload_archive = value.get("upload_archive")
+        should_upload = any(
+            field is not None
+            for field in [
+                upload_name,
+                upload_path,
+                upload_days,
+                upload_archive,
+            ]
+        )
+
+        if should_upload:
+            if upload_archive is None:
+                upload_archive = True
+
+            if upload_days is None:
+                upload_days = 90
+
+            if upload_path is None:
+                raise ValueError("upload_path must be defined when uploading")
+
+            if upload_archive and upload_name is None:
+                raise ValueError(
+                    "upload_name must be defined when uploading a zip archive"
+                )
+
         try:
             job = cls(
                 name=value["name"],
                 gradle_args=value["gradle_args"],
-                upload_name=value.get("upload_name"),
-                upload_path=value.get("upload_path"),
-                upload_days=value.get("upload_days"),
+                upload_name=upload_name,
+                upload_path=upload_path,
+                upload_days=upload_days,
+                upload_archive=upload_archive,
             )
         except KeyError as e:
             raise ValueError(f"MatrixJob missing required key {e.args[0]}") from None
@@ -34,11 +65,5 @@ class MatrixJob:
 
         if not isinstance(job.gradle_args, list):
             raise ValueError("gradle_args must be a list")
-
-        if (job.upload_name is None) ^ (job.upload_path is None):
-            raise ValueError("upload_name and upload_path must be defined together")
-
-        if job.upload_days is not None and job.upload_name is None:
-            raise ValueError("upload_days requires upload_name and upload_path")
 
         return job
--- ci/test/test_matrix_model.py
+++ ci/test/test_matrix_model.py
@@ -5,41 +5,130 @@ from freecam_ci.matrix_model import MatrixJob
 
 def test_matrixjob_to_dict():
     job = MatrixJob(
-        name="Build test",
-        gradle_args=[":common:test"],
-        upload_name=None,
+        name="Build thing",
+        gradle_args=[":common:thing"],
+        upload_name="Thing",
         upload_path="build/libs/*.jar",
-        upload_days=None,
+        upload_days=90,
+        upload_archive=True,
     )
     assert job.to_dict() == {
-        "name": "Build test",
-        "gradle_args": [":common:test"],
+        "name": "Build thing",
+        "gradle_args": [":common:thing"],
+        "upload_name": "Thing",
         "upload_path": "build/libs/*.jar",
+        "upload_days": 90,
+        "upload_archive": True,
     }
 
 
 def test_matrixjob_from_dict():
     value = {
-        "name": "Build test",
-        "gradle_args": [":common:test"],
+        "name": "Build thing",
+        "gradle_args": [":common:thing"],
         "upload_path": "build/libs/*.jar",
         "upload_name": "artifact",
     }
     job = MatrixJob.from_dict(value)
     assert job == MatrixJob(
-        name="Build test",
-        gradle_args=[":common:test"],
+        name="Build thing",
+        gradle_args=[":common:thing"],
         upload_name="artifact",
         upload_path="build/libs/*.jar",
-        upload_days=None,
+        upload_days=90,
+        upload_archive=True,
     )
 
 
-def test_matrixjob_from_dict_upload_pair_required():
+def test_matrixjob_to_dict_no_archive():
+    job = MatrixJob(
+        name="Build file",
+        gradle_args=[":common:file"],
+        upload_name=None,
+        upload_path="build/libs/*.jar",
+        upload_days=90,
+        upload_archive=False,
+    )
+    assert job.to_dict() == {
+        "name": "Build file",
+        "gradle_args": [":common:file"],
+        "upload_path": "build/libs/*.jar",
+        "upload_days": 90,
+        "upload_archive": False,
+    }
+
+
+def test_matrixjob_from_dict_no_archive():
+    value = {
+        "name": "Build file",
+        "gradle_args": [":common:file"],
+        "upload_path": "build/libs/*.jar",
+        "upload_archive": False,
+    }
+    job = MatrixJob.from_dict(value)
+    assert job == MatrixJob(
+        name="Build file",
+        gradle_args=[":common:file"],
+        upload_name=None,
+        upload_path="build/libs/*.jar",
+        upload_days=90,
+        upload_archive=False,
+    )
+
+
+def test_matrixjob_to_dict_no_upload():
+    job = MatrixJob(
+        name="Build test",
+        gradle_args=[":common:test"],
+    )
+    assert job.to_dict() == {
+        "name": "Build test",
+        "gradle_args": [":common:test"],
+    }
+
+
+def test_matrixjob_from_dict_no_upload():
+    value = {
+        "name": "Build test",
+        "gradle_args": [":common:test"],
+    }
+    job = MatrixJob.from_dict(value)
+    assert job == MatrixJob(
+        name="Build test",
+        gradle_args=[":common:test"],
+        upload_name=None,
+        upload_path=None,
+        upload_days=None,
+        upload_archive=None,
+    )
+
+
+def test_matrixjob_from_dict_upload_name_required():
     value = {
         "name": "Build test",
         "gradle_args": [":common:test"],
         "upload_path": "build/libs/*.jar",
+    }
+    with pytest.raises(ValueError):
+        MatrixJob.from_dict(value)
+
+
+def test_matrixjob_from_dict_upload_path_required():
+    value = {
+        "name": "Build test",
+        "gradle_args": [":common:test"],
+        "upload_name": "build/libs/*.jar",
+    }
+    with pytest.raises(ValueError):
+        MatrixJob.from_dict(value)
+
+
+def test_matrixjob_from_dict_upload_path_required_no_archive():
+    value = {
+        "name": "Build test",
+        "gradle_args": [":common:test"],
+        "upload_name": "build/libs/*.jar",
+        "upload_archive": False,
     }
     with pytest.raises(ValueError):
         MatrixJob.from_dict(value)
