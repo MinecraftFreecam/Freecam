a5041678e4e76392e79ec86950a8a96c29b20f6d
personalized-advertising <265311956+personalized-advertising@users.noreply.github.com>
2026-07-22 22:44:54 +0100

    Update FreecamFabric.java

--- fabric/src/main/java/net/xolt/freecam/fabric/FreecamFabric.java
+++ fabric/src/main/java/net/xolt/freecam/fabric/FreecamFabric.java
@@ -11,6 +11,7 @@ public class FreecamFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModConfig.setup();
+        FabricServerPolicyNetworking.register();
         ModBindings.forEach(KeyMappingHelper::registerKeyMapping);
         ClientTickEvents.START_CLIENT_TICK.register(Freecam::preTick);
         ClientTickEvents.END_CLIENT_TICK.register(Freecam::postTick);
