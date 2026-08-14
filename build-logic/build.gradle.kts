95d597add7901f356e4237f6b51e7f0ff0306552
Matt Sturgeon <matt@sturgeon.me.uk>
2026-08-15 00:06:08 +0100

    build: remove `:test` & `:check` aggregation
    While this is useful for `:build-logic:test`, it is not compatible with
    Gradle's Configure on Demand.
    
    Gradle's native task recursion is better anyway:
        --project-dir build-logic test
    will find all `:test` tasks in subprojects.

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,19 +1,3 @@
 plugins {
     `kotlin-dsl` apply false
 }
-
-tasks.register("check") {
-    description = "Runs all checks in subprojects"
-    aggregateByName()
-}
-
-tasks.register("test") {
-    description = "Runs the test suite in subprojects"
-    aggregateByName()
-}
-
-fun Task.aggregateByName() {
-    dependsOn(provider {
-        subprojects.mapNotNull { it.tasks.findByName(name) }
-    })
-}
