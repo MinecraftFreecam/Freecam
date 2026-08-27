a52168f8c36b15fb5960bf85bf38b12a0e678787
Matt Sturgeon <matt@sturgeon.me.uk>
2026-08-27 20:05:21 +0100

    refactor(branding): store the icon monochrome
    Apply colours via stylesheets & explicit backgrounds, meaning the
    canonical icon shape can be monochrome and inherit `currentColor`.

--- branding/build.gradle.kts
+++ branding/build.gradle.kts
@@ -1,19 +1,62 @@
 import org.gradle.internal.DefaultTaskExecutionRequest
+import java.awt.Color
 
 plugins {
     id("freecam.svg")
 }
 
 val sourceDir = layout.projectDirectory.dir("src/main")
+val iconBackground = Color(0x002a36)
+val iconForeground = Color(0x00607c)
+
+val Color.xmlHexRgb
+    get() = (rgb and 0xffffff).toHexString(HexFormat {
+        number {
+            prefix = "#"
+            minLength = 6
+            removeLeadingZeros = true
+        }
+    })
+
+val styleSheetTask = tasks.register("iconStyleSheet") {
+    description = "Generate the icon's CSS stylesheet"
+    inputs.properties("foreground" to iconForeground)
+    outputs.file(layout.buildDirectory.file("resources/icon.css"))
+    doLast {
+        outputs.files.singleFile.apply {
+            writeText("svg { color: ${iconForeground.xmlHexRgb}; }")
+        }
+    }
+}
 
 svg.exportPngMatrix("icon", 24, 32, 48, 64, 96, 100, 128, 192, 256, 512, 1024) {
     source = sourceDir.file("icon.svg")
+    userStylesheet = styleSheetTask.map { it.outputs.files.singleFile }
+    backgroundColor = iconBackground
 }
 
 val installProjectIconTask = tasks.register<Copy>("installProjectIcon") {
     description = "Install the IntelliJ IDEA project icon"
     from(sourceDir.file("icon.svg"))
     into(rootDir.resolve(".idea"))
+
+    inputs.properties(
+        "background" to iconBackground,
+        "foreground" to iconForeground,
+    )
+
+    filter { line ->
+        when {
+            line.startsWith("<svg ") ->
+                """
+                |$line
+                |  <rect width="100%" height="100%" fill="${iconBackground.xmlHexRgb}"/>
+                """.trimMargin()
+            line.contains("\"currentColor\"") ->
+                line.replace("\"currentColor\"", "\"${iconForeground.xmlHexRgb}\"")
+            else -> line
+        }
+    }
 }
 
 // Hack to make IntelliJ IDEA run installProjectIconTask on sync
