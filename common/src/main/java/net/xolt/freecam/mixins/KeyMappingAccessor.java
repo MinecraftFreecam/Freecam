fb2733e0e1f203fa73e8981e58d6506d066f70e9
reusteur73 <kahlumba@pm.me>
2026-08-31 10:38:27 +1100

    fix: stop freecam from sinking when toggle sneak is enabled

---
+++ common/src/main/java/net/xolt/freecam/mixins/KeyMappingAccessor.java
@@ -0,0 +1,13 @@
+package net.xolt.freecam.mixins;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(KeyMapping.class)
+public interface KeyMappingAccessor {
+
+    @Accessor("key")
+    InputConstants.Key freecam$getKey();
+}
